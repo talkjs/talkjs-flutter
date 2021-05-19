d4dfbc4e4df9f9caf916ddb6b3a50e25db866b6f
Victor Omondi <victor@slick.co.ke>
2021-05-19 13:22:06 +0300

    Implement test for oneOnOneId method
    Signed-off-by: Victor Omondi <victor@slick.co.ke>

--- talkjs/test/talkjs_test.dart
+++ talkjs/test/talkjs_test.dart
@@ -3,10 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:talkjs/talkjs.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('test oneOnOneId', () {
+    expect(Talk.oneOnOneId('1234', 'abcd'), '35ec37e6e0ca43ac8ccc');
+    expect(Talk.oneOnOneId('abcd', '1234'), '35ec37e6e0ca43ac8ccc');
   });
 }
