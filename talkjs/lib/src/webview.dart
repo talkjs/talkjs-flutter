1d6b7a7ef0aa185beb98750d1a2815250efbf211
Victor Omondi <victor@slick.co.ke>
2021-05-19 11:20:57 +0300

    Implement ChatWebView widget
    This widget acts like a simple wrapper around the WebView widget provided by
    webview_flutter.
    
    Signed-off-by: Victor Omondi <victor@slick.co.ke>

---
+++ talkjs/lib/src/webview.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ChatWebView extends StatefulWidget {
+  final ChatWebViewState state;
+
+  ChatWebView(WebViewCreatedCallback webViewFn, PageFinishedCallback jsFn)
+      : state = ChatWebViewState(webViewFn, jsFn);
+
+  @override
+  ChatWebViewState createState() => this.state;
+}
+
+class ChatWebViewState extends State<ChatWebView> {
+  late WebView webView;
+
+  ChatWebViewState(WebViewCreatedCallback webViewFn,
+      PageFinishedCallback jsFn) {
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+
+    this.webView = WebView(
+      initialUrl: '',
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: webViewFn,
+      onPageFinished: jsFn,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return this.webView;
+  }
+}
