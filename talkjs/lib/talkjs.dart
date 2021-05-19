f15e8bb7fd79dfbda90722ed7c4dc45ebc05f9c9
Victor Omondi <victor@slick.co.ke>
2021-05-19 11:09:42 +0300

    Implement Talk class
    Signed-off-by: Victor Omondi <victor@slick.co.ke>

--- talkjs/lib/talkjs.dart
+++ talkjs/lib/talkjs.dart
@@ -1,7 +1,17 @@
 library talkjs;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert' show json, utf8;
+import 'package:crypto/crypto.dart' show sha1;
+
+class Talk {
+  static String oneOnOneId(String me, String other) {
+    List ids = [me, other];
+    ids.sort();
+
+    final encoded = json.encode(ids);
+    final digest =  sha1.convert(utf8.encode(encoded));
+
+    final hash = digest.toString().toLowerCase();
+    return hash.substring(0, 20);
+  }
 }
