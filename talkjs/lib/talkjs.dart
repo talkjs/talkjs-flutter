a0e45f3534475fffb17416dbe1049d40ca5ab116
Victor Omondi <victor@slick.co.ke>
2021-05-19 12:46:48 +0300

    Fix missing chatoptions export
    Also rearranged the order of the exports to be alphabetical
    
    Signed-off-by: Victor Omondi <victor@slick.co.ke>

--- talkjs/lib/talkjs.dart
+++ talkjs/lib/talkjs.dart
@@ -3,11 +3,12 @@ library talkjs;
 import 'dart:convert' show json, utf8;
 import 'package:crypto/crypto.dart' show sha1;
 
-export 'src/ui.dart';
+export 'src/chatoptions.dart';
 export 'src/conversation.dart';
+export 'src/session.dart';
+export 'src/ui.dart';
 export 'src/user.dart';
 export 'src/webview.dart';
-export 'src/session.dart';
 
 class Talk {
   static String oneOnOneId(String me, String other) {
