b9b1a973c0ab59eb6502f4b50f39e041b6af277f
Victor Omondi <victor@slick.co.ke>
2022-06-06 14:47:10 +0300

    Revert removal of firebase_options.dart
    Someone using the example app and is only interested in iOS may not
    have flutterfire installed hence this file may not be generated. To
    prevent the app from crashing in that case, we provide this

--- examples/push_notifications/lib/main.dart
+++ examples/push_notifications/lib/main.dart
@@ -5,9 +5,12 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+  if (Platform.isAndroid) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 
   await Talk.registerPushNotificationHandlers(
     androidChannel: const AndroidChannel(
